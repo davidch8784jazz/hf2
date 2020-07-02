b5f2348b9eeecb3ce5c50335baa310747697e76d
davidch8784jazz <davidch8784@gmail.com>
2020-07-02 23:02:14 +0900

    임포트 삭제

--- src/main/java/kr/co/hatchfly/web/IndexController.java
+++ src/main/java/kr/co/hatchfly/web/IndexController.java
@@ -1,19 +1,15 @@
 package kr.co.hatchfly.web;
 
-import com.sun.xml.internal.ws.developer.Serialization;
 import kr.co.hatchfly.config.auth.LoginUser;
 import kr.co.hatchfly.config.auth.dto.SessionUser;
-import kr.co.hatchfly.domain.user.User;
 import kr.co.hatchfly.service.posts.PostsService;
 import kr.co.hatchfly.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
